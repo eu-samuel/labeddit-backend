b56c2311a8d764898d011c04f5c70c6f7c2ea463
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-10-09 06:16:34 -0300

    Update CommentController.ts

--- src/controller/CommentController.ts
+++ src/controller/CommentController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express"
 import { CommentBusiness } from "../business/CommentBusiness"
 import { BaseError } from "../errors/BaseError"
 import { ZodError } from "zod"
-import { EditCommentSchema } from "../dtos/Comments/editComment.dto"
 import { GetCommentsSchema } from "../dtos/Comments/getComments.dto"
 import { CreateCommentSchema } from "../dtos/Comments/createComment.dto"
 import { DeleteCommentSchema } from "../dtos/Comments/deleteComment.dto"
@@ -60,33 +59,6 @@ export class CommentController {
       }
     }
   }
-
-  public editCommentByid = async (req: Request, res: Response) => {
-    try {
-
-      const input = EditCommentSchema.parse({
-        idToEdit: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        token: req.headers.authorization
-      })
-
-      const output = await this.commentBusiness.editComment(input)
-
-      res.status(200).send(output)
-    } catch (error) {
-      console.log(error)
-
-      if (error instanceof ZodError) {
-        res.status(400).send(`${error.issues[0].path}: ${error.issues[0].message}`)
-      } else if (error instanceof BaseError) {
-        res.status(error.statusCode).send(error.message)
-      } else {
-        res.status(500).send("Erro inesperado")
-      }
-    }
-  }
-
 
   public deleteCommentById = async (req: Request, res: Response) => {
     try {
