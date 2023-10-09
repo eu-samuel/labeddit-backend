50f3f337544f19c9cbe6c12137cf5b57cef54cc3
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-10-09 06:44:32 -0300

    Update UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -7,7 +7,6 @@ import { SignupSchema } from "../dtos/Users/signup.dto"
 import { LoginSchema } from "../dtos/Users/login.dto"
 import { GetUsersSchema } from "../dtos/Users/getUsers.dto"
 import { DeleteUserSchema } from "../dtos/Users/deleteUser.dto"
-import { ChangeUserRoleSchema } from "../dtos/Users/changeUserRole.dto"
 import { NotFoundError } from "../errors/NotFoundError"
 
 export class UserController {
@@ -148,36 +147,6 @@ export class UserController {
         res.status(400).send(`${error.issues[0].path}: ${error.issues[0].message}`)
       } else if (error instanceof BaseError) {
         res.status(error.statusCode).send(error.message)
-      }
-    }
-  }
-
-  public editUserRole = async (req: Request, res: Response) => {
-    try {
-
-      const input = ChangeUserRoleSchema.parse({
-        idToEdit: req.params.id,
-        role: req.body.role,
-        token: req.headers.authorization
-      })
-
-      const output = await this.userBusiness.editUserRoleById(input)
-
-      if (!output) {
-        res.statusCode = 400
-        throw new NotFoundError()
-      }
-
-      res.status(200).send(output)
-    } catch (error) {
-      console.log(error)
-
-      if (error instanceof ZodError) {
-        res.status(400).send(`${error.issues[0].path}: ${error.issues[0].message}`)
-      } else if (error instanceof BaseError) {
-        res.status(error.statusCode).send(error.message)
-      } else {
-        res.status(500).send("Erro inesperado")
       }
     }
   }
